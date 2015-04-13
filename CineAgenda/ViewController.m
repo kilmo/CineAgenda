74ae88f5cbf13830ce278c09e788fc8811e45668
erikamoriya <erikamoriya@Erikas-MacBook-Pro.local>
2015-04-13 09:54:16 -0300

    Initial Commit

---
+++ CineAgenda/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CineAgenda
+//
+//  Created by Érika Tiemi Uehara Moriya on 4/13/15.
+//  Copyright (c) 2015 LLBE Corp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
